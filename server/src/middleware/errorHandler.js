13df771c396af53931d9a40c3369119f345c37c9
Divyesh Radadiya <120237020+divyeshradadiya@users.noreply.github.com>
2023-05-23 18:20:41 +0530

    Update errorHandler.js
    i improved some codes for better quality..
    
    here is what i added : 
    
        The code checks if the token starts with "Bearer " (including a space) to ensure correct parsing of the token.
    
        Specific error cases such as expired tokens or invalid tokens are handled separately, returning appropriate error responses.
    
        The code assumes that the JWT_SECRET environment variable is already properly set.
    
    hope you like it.

--- server/src/middleware/errorHandler.js
+++ server/src/middleware/errorHandler.js
@@ -1,15 +1,32 @@
-const notFound = (req, res, next) => {
-  res.status(404);
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  next(error);
+import jwt from "jsonwebtoken";
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(401).send("Unauthorized User");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      const strippedToken = token.slice(7).trim();
+      const verified = jwt.verify(strippedToken, process.env.JWT_SECRET);
+      req.user = verified;
+      return next();
+    }
+
+    return res.status(401).send("Invalid Token");
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ error: "Token expired" });
+    }
+
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  res.status(statusCode);
-  res.json({
-    error: err.message || "Internal server error!",
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
-export { notFound, errorHandler };
+export default verifyToken;
